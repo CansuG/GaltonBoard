7373ee279e77254813ca4c818d81f7e70e9e95bf
CansuG <cansugurel@eskisehir.edu.tr>
2022-05-05 21:05:05 +0300

    .

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    static int[] array;
+
+    public static void main(String[] args) {
+
+        int numBins = Integer.parseInt(args[1]);
+        int numThreads = Integer.parseInt(args[0]);
+        int galtonBoardsThread = 0;
+
+        array = new int[numBins];
+
+        GaltonBoard galtonBoard = new GaltonBoard(numBins);
+
+        for (int i = 0; i < numThreads; i++) {
+
+            Thread thread = new Thread(galtonBoard);
+            thread.start();
+
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            galtonBoardsThread += array[i];
+            System.out.println((i + 1) + "      " + array[i]);
+        }
+
+
+        System.out.println("Number of requested thread: "+ numThreads);
+        System.out.println("Sum of bin values : "+ galtonBoardsThread);
+
+    }
+}
